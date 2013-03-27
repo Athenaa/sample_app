71d22c1d60c77cf793bdb9e87b97e849cf1cdae1
Athenaa <Athenaa@telenav.com>
2013-03-27 15:29:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aa2aef52927cd5c69e7e13eec01c4ac962fde6dada646952616196a41e18aeb971421c79d2f2d5531ad78e6a8c97830ccb31326a19344355a1f4688f48fc772a'
